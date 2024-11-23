f9d852cb9d25914aff70f82fb53f7d3c0e014aa1
Ali Gaziev <67166171+GazievAli@users.noreply.github.com>
2024-11-23 22:03:13 +0300

    DataBase finish

--- tictactoe/src/app/page.tsx
+++ tictactoe/src/app/page.tsx
@@ -1,9 +1,23 @@
-import {Button} from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
+import { prisma } from "@/shared/lib/db";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
+
   return (
     <div>
-      <Button size="lg" variant={"destructive"}>Hello!</Button>
+      <Button size="lg" variant={"destructive"}>
+        Hello!
+      </Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
